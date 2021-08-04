bc85f701273b10575e5052380af801a422a81597
oisin-h011 <51450044+oisin-h011@users.noreply.github.com>
2021-08-04 11:35:09 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r00100051",
+      "name": "R0010005[1]",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -3.070614267931349,
+        "pitch": 0.17652565105741758,
+        "fov": 1.4450773171985711
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.099639747501735,
+          "pitch": 0.5109048243952579,
+          "rotation": 0,
+          "target": "1-r00100061"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-r00100061",
+      "name": "R0010006[1]",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.4450773171985711
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.07756416091754836,
+          "pitch": 0.4496572680544144,
+          "rotation": 18.84955592153877,
+          "target": "0-r00100051"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
